3862140030695abaf6c9c94758cfab8d0756427e
Fabiha Alam <alomfabiha@gmail.com>
2024-10-08 11:13:15 +0600

    MultiThread Syncronization

---
+++ class_5/Class_1/src/thread_1/MainThread.java
@@ -0,0 +1,15 @@
+package thread_1;
+
+public class MainThread {
+    public static void main(String[] args) {
+        RoomInfo roomInfo = new RoomInfo();
+
+        Thread1 thread1 = new Thread1(roomInfo,"Shamim",1);
+        Thread2 thread2 = new Thread2(roomInfo,"Rakibul",1);
+
+        thread1.start();
+        thread2.start();
+    }
+
+
+}
---
+++ class_5/Class_1/src/thread_1/RoomInfo.java
@@ -0,0 +1,18 @@
+package thread_1;
+
+public class RoomInfo {
+	   int totalSeat =1;
+	    public synchronized void seatAssign(String name,int seat){
+
+	        if(totalSeat >= seat){
+	            System.out.println("This seat Assign by "+name);
+	            totalSeat = totalSeat -seat;
+	        }
+	        else
+	        {
+	            System.out.println("No seat availavle");
+	        }
+
+	    }
+
+}
